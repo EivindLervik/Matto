453021d728cdb54a56217949cbf8f75a7730cd2d
EivindLervik <eivind_fantaboy@msn.com>
2017-02-08 00:25:30 +0100

    Made the list functioning

---
+++ Assets/Script/ListPopulatingScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ListPopulatingScript : MonoBehaviour {
+
+    public int amount;
+    public float spaceBetween;
+    public float indentFromEdges;
+    public float height;
+    public GameObject item;
+
+    private ArrayList items;
+
+	void Start () {
+        RectTransform rtSelf = GetComponent<RectTransform>();
+        rtSelf.offsetMin = new Vector2(0.0f, (-amount * (spaceBetween + height)) - indentFromEdges);
+
+
+        for (int i=0; i<amount; i++)
+        {
+            GameObject go = Instantiate(item, transform);
+            RectTransform rt = go.GetComponent<RectTransform>();
+
+            rt.offsetMin = new Vector2(indentFromEdges, -indentFromEdges - ((height * (i + 1)) + (spaceBetween * i)));
+            rt.offsetMax = new Vector2(-indentFromEdges, -indentFromEdges - ((height + spaceBetween) * i));
+            
+        }
+	}
+
+	void Update () {
+		
+	}
+}
